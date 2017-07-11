6816e4f847563cabf3d7be64ff63eaa99277422d
fabjohn <jyang94@illinois.edu>
2017-07-11 09:15:52 -0500

    Finished ROC visualization

--- faceAPI/ROC.py
+++ faceAPI/ROC.py
@@ -5,15 +5,21 @@ import json, codecs
 import matplotlib.pyplot as plt
 import cv2 
 import time
-import numpy as numpy
+import numpy as np
 from sklearn.datasets import fetch_lfw_people
+import os
+import sys
 
 
 def main():
 	key = '7a620905befc45e5adbfef7b16b56b01'
 	##Fetech data from LFW dataset. 
-
-	lfw_people = fetch_lfw_people(min_faces_per_person = 15, resize = None)
+	person_size = 100	##5 times
+	fold_n = int(person_size / 5)
+	print(fold_n)
+	individual_size = 2
+	z=9
+	lfw_people = fetch_lfw_people(min_faces_per_person = 10, resize = None)
 	print(len(lfw_people.images))
 	#print(len(lfw_people.target))
 	print(len(lfw_people.target_names))
@@ -25,34 +31,43 @@ def main():
 	test_y = []	##test binary data
 	temp = [0]*5000
 	for i in range(len(lfw_people.target)):
-		if lfw_people.target[i] < 10:
+		if lfw_people.target[i] < person_size:
 			if temp[lfw_people.target[i]] <1:	
 				train_x.append(lfw_people.target_names[lfw_people.target[i]])
 				train_y.append(lfw_people.binary[i])
-			elif 0<temp[lfw_people.target[i]]<4:
+			elif 0<temp[lfw_people.target[i]]<individual_size:
 				test_x.append(lfw_people.target_names[lfw_people.target[i]])
 				test_y.append(lfw_people.binary[i])
 			temp[lfw_people.target[i]] += 1
 
-	print(train_x)
-	print('Let the fun begin!')
 
-	## start training request
-	## first create person group for first 5 persons
+	print(train_x)
+	print(len(train_x))
+	print('Let the fun begin!')
+	#print(train_y)
+	# faceId = detectFace(test_y[0], key)
+	# x, y, z = identifyFace(faceId, key, '1test1')
+	# print(x)
+	# print(y)
+	# print(z)
+
+	# start training request
+	# first create person group for first 5 persons
 	cnt = 0
 	nameId = {}
-	for i in range(2):
-		groupId = 'test1%s'%i
+	for i in range(fold_n):
+		groupId = '%stest%s'%(z,i)
 		createPersonGroups(groupId, key)
 
 		## Create persons 5 per group
-		for i in range(5):
-			curr_label = train_x[i+cnt]
-			personId = createPerson(groupId, key, curr_label)
+		for j in range(5):
+
+			curr_label = train_x[j+cnt]
+			personId = createPerson(groupId, key, curr_label) 		##possibility to fail due to multiple face. Need face crop
 			nameId[curr_label] = personId
 			## Load faces into the dataset
-			curr_binary = train_y[i+cnt]
-			addPersonFace(groupId, key, curr_label, curr_binary)
+			curr_binary = train_y[j+cnt]
+			addPersonFace(groupId, key, personId, curr_binary)
 
 		##train person groups
 		trainPersonGroup(groupId, key)
@@ -62,10 +77,11 @@ def main():
 	confidenceLists = []
 	for item in test_y:
 		faceId = detectFace(item, key)
+		print(faceId)
 		maxConf = 0
 		temp = []
-		for i in range(2)
-			groupId = 'test1%s'%i
+		for i in range(fold_n):
+			groupId = '%stest%s'%(z,i)
 			bestPId, bestConf, confList  = identifyFace(faceId, key, groupId)
 			if bestConf > maxConf:
 				predictId = bestPId
@@ -75,11 +91,74 @@ def main():
 		predictList.append(predictId)
 		confidenceLists.append(temp)
 
-	for thrshld in range(11):
-		thrshld = thrshld / 10
-		
+	if len(predictList) != len(test_x):
+		print('this is not right. fuck')
+	cnt = 0
+	for i in range(len(predictList)):
+		if predictList[i] == nameId[test_x[i]]:
+			cnt +=1
+	accuracy = cnt/len(predictList)
+	print(accuracy)
+	# print(len(predictList))
+	# print(predictList)
+	#print(confidenceLists)
+	##genereate ROC curve
+	generateRoc(predictList, test_x, confidenceLists, nameId)
 
 
+
+
+def generateRoc(predictList,test_data, confidenceLists, nameId):
+	fprs = []
+	tprs = []
+	for thrshld in range(101):
+		tp = tn = fp = fn = 0
+		thrshld = thrshld / 100
+		for i in range(len(predictList)):
+			curr_Conf = confidenceLists[i]
+			sum_true = sum(i >= thrshld for i in curr_Conf)
+			predict_same = predictList[i] == nameId[test_data[i]]
+			if sum_true>0 and predict_same:
+				tp += 1
+				fp += sum_true - 1
+				tn += len(curr_Conf) - sum_true
+			elif sum_true>0 and not predict_same:
+				fp += sum_true
+				tn += len(curr_Conf) - sum_true
+			elif sum_true<= 0 and predict_same:
+				fn += 1
+				tn += len(curr_Conf) - 1
+			elif sum_true<= 0 and not predict_same:
+				tn += len(curr_Conf)
+		print(fn)
+		if tp + fn ==0:
+			tpr = 0
+		else:
+			tpr = float(tp) / float(tp+fn)
+		if fp + tn == 0:
+			fpr = 0
+		else:
+			fpr = float(fp) / float(fp+tn)
+		fprs.append(fpr)
+		tprs.append(tpr)
+	print(tprs)
+	print(fprs)
+	AUC = getAUC(fprs, tprs)
+	print(AUC)
+	plt.plot(fprs, tprs)
+	plt.xlabel('fpr')
+	plt.ylabel('tpr')
+	plt.title('ROC curve')
+	plt.show()
+
+def getAUC(fprs, tprs):
+    sortedFprs, sortedTprs = zip(*sorted(zip(*(fprs, tprs))))
+    sortedFprs = list(sortedFprs)
+    sortedTprs = list(sortedTprs)
+    if sortedFprs[-1] != 1.0:
+        sortedFprs.append(1.0)
+        sortedTprs.append(sortedTprs[-1])
+    return np.trapz(sortedTprs, sortedFprs)
 
 
 def createPersonGroups(groupId, key):
@@ -125,7 +204,7 @@ def createPerson(groupId, key, name):
 		personId = jdata['personId']
 		#nameId[curr] = personId
 		#cntId[name] = 3
-		print(curr, ':', personId)
+		print(name, ':', personId)
 		conn.close()
 
 	except Exception as e:
@@ -193,37 +272,28 @@ def detectFace(binary_data, key):
 	    'returnFaceAttributes': 'age,gender,glasses',   
 	})
 	try:
-	    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-	    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-	    response = conn.getresponse()
-	    reader = codecs.getreader('utf-8')
-	    data = response.read().decode()
-	    #jdata for index json file
-	    jdata = json.loads(data)
-	    print(jdata)
-	    #print(jdata[1])
-	    #print(jdata[0]['faceRectangle'])
-	    faceId  = []
-	    #For loop for display bouding box 
-	    
-	    min_value = 0
-	    for face in jdata:
-
-	        temp = face['faceRectangle']
-	        #cv2.rectangle(img,(temp['left'],temp['top']),(temp['left']+temp['width'] ,  temp['top']+temp['height']), (0,255,0),2)
-	    	
-	    	if temp['top']*temp['width'] > min_value:
-	    		min_value = temp['top']*temp['width']						##only the largest face get identified. 
-	    		max_face = face['faceId']
-	        #faceId.append(face['faceId'])
-
-
-	    #print(data['faceId'])
-	    #print(len(data[0]))
-
-	    conn.close()
+		conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+		conn.request("POST", "/face/v1.0/detect?%s" % params, binary_data, headers)
+		response = conn.getresponse()
+		reader = codecs.getreader('utf-8')
+		data = response.read().decode()
+		#jdata for index json file
+		jdata = json.loads(data)
+		print(jdata)
+		#print(jdata[1])
+		#print(jdata[0]['faceRectangle'])
+		faceId  = []
+		#For loop for display bouding box 
+		min_value = 0
+		for face in jdata:
+			temp = face['faceRectangle']
+			#cv2.rectangle(img,(temp['left'],temp['top']),(temp['left']+temp['width'] ,  temp['top']+temp['height']), (0,255,0),2)
+			if temp['top']*temp['width'] > min_value:
+				min_value = temp['top']*temp['width']						##only the largest face get identified. 
+				max_face = face['faceId']
+		conn.close()
 	except Exception as e:
-	    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 	return max_face
 
@@ -244,7 +314,7 @@ def identifyFace(faceId, key, groupId):
             'maxNumOfCandidatesReturned': 5,
             'confidenceThreshold' : 0
     }
-    conflist = []
+    confList = []
     n=0
     try:
         conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
@@ -252,14 +322,17 @@ def identifyFace(faceId, key, groupId):
         response = conn.getresponse()
         data = response.read().decode()
         jdata = json.loads(data)
-        temp = jdata['candidates']
-        for person in temp:
+        print(jdata)
+        if 'candidates' not in jdata[0]:
+        	print('shit is not right')
+        temp = jdata[0]
+        for person in temp['candidates']:
         	if n == 0:
         		mId = person['personId']			##max conf 
         		mConf = person['confidence']
         	confList.append(person['confidence'])   ##only 5 
         	n += 1
-        print(data)
+#        print(data)
         conn.close()
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
